85e7a29a12303c40ade7a555a9231374ec1a5af3
Oral Kaplan <oralkaplan@gmail.com>
2020-09-28 18:37:13 +0900

    Updates

---
+++ precache-manifest.f0d26b7a1d252896bdb2ac53d12c48f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c7a717c968b901552e61b37c9c913a",
+    "url": "/oralkaplan.github.io/index.html"
+  },
+  {
+    "revision": "7d73d19d99ae1094526b",
+    "url": "/oralkaplan.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98f1f81283a43cf03587",
+    "url": "/oralkaplan.github.io/static/js/2.b398c629.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oralkaplan.github.io/static/js/2.b398c629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d73d19d99ae1094526b",
+    "url": "/oralkaplan.github.io/static/js/main.49d11d03.chunk.js"
+  },
+  {
+    "revision": "dfa4f4d6dc1f09548d26",
+    "url": "/oralkaplan.github.io/static/js/runtime-main.6744b15b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oralkaplan.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
